9e5714f7694296ed778d442e422cd26a0fe31cea
LenaSaraieva <lena.saraeva1904@gmail.com>
2023-11-29 15:33:00 +0200

    first branch

---
+++ script.js
@@ -0,0 +1,31 @@
+/* Переписать switch case на if else
+
+var name = prompt("Введите ваше имя");
+
+switch (name) {
+  case 'Денис':
+    alert( 'Добрый день, учитель' );
+    break;
+
+  case 'Алиса':
+  case 'Иван':
+  case 'Анастасия':
+  case 'Андрей':
+    alert( 'Привет, одногрупник' );
+    break;
+
+  default:
+    alert( 'Ты не из нашей песочницы!' );
+} */
+
+let userName = prompt("Введите ваше имя");
+
+if ( userName == Denis ) {
+    alert( 'Добрый день, учитель' );
+}
+else if ( userName == Alisa || userName == Ivan || userName == Anastasia || userName == Andrew ) {
+    alert( 'Привет, одногрупник' );
+}
+else {
+    alert( 'Ты не из нашей песочницы!' );
+}
